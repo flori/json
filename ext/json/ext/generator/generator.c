08f79bb4f16e5dcfe059da0de45759d2904c363f
Yusuke Endoh <mame@ruby-lang.org>
2023-12-27 15:48:17 +0900

    Stop prebuilding object_delim
    This speeds up `JSON.generate` by about 4% in a benchmark

--- ext/json/ext/generator/generator.c
+++ ext/json/ext/generator/generator.c
@@ -610,7 +610,6 @@ static void State_free(void *ptr)
     if (state->space_before) ruby_xfree(state->space_before);
     if (state->object_nl) ruby_xfree(state->object_nl);
     if (state->array_nl) ruby_xfree(state->array_nl);
-    if (state->object_delim) fbuffer_free(state->object_delim);
     if (state->object_delim2) fbuffer_free(state->object_delim2);
     ruby_xfree(state);
 }
@@ -624,7 +623,6 @@ static size_t State_memsize(const void *ptr)
     if (state->space_before) size += state->space_before_len + 1;
     if (state->object_nl) size += state->object_nl_len + 1;
     if (state->array_nl) size += state->array_nl_len + 1;
-    if (state->object_delim) size += FBUFFER_CAPA(state->object_delim);
     if (state->object_delim2) size += FBUFFER_CAPA(state->object_delim2);
     return size;
 }
@@ -843,15 +841,13 @@ json_object_i(VALUE key, VALUE val, VALUE _arg)
     long object_nl_len = state->object_nl_len;
     char *indent = state->indent;
     long indent_len = state->indent_len;
-    char *delim = FBUFFER_PTR(state->object_delim);
-    long delim_len = FBUFFER_LEN(state->object_delim);
     char *delim2 = FBUFFER_PTR(state->object_delim2);
     long delim2_len = FBUFFER_LEN(state->object_delim2);
     long depth = state->depth;
     int j;
     VALUE klass, key_to_s;
 
-    if (arg->iter > 0) fbuffer_append(buffer, delim, delim_len);
+    if (arg->iter > 0) fbuffer_append_char(buffer, ',');
     if (object_nl) {
         fbuffer_append(buffer, object_nl, object_nl_len);
     }
@@ -1070,12 +1066,6 @@ static FBuffer *cState_prepare_buffer(VALUE self)
     GET_STATE(self);
     buffer = fbuffer_alloc(state->buffer_initial_length);
 
-    if (state->object_delim) {
-        fbuffer_clear(state->object_delim);
-    } else {
-        state->object_delim = fbuffer_alloc(16);
-    }
-    fbuffer_append_char(state->object_delim, ',');
     if (state->object_delim2) {
         fbuffer_clear(state->object_delim2);
     } else {
@@ -1163,7 +1153,6 @@ static VALUE cState_init_copy(VALUE obj, VALUE orig)
     objState->space_before = fstrndup(origState->space_before, origState->space_before_len);
     objState->object_nl = fstrndup(origState->object_nl, origState->object_nl_len);
     objState->array_nl = fstrndup(origState->array_nl, origState->array_nl_len);
-    if (origState->object_delim) objState->object_delim = fbuffer_dup(origState->object_delim);
     if (origState->object_delim2) objState->object_delim2 = fbuffer_dup(origState->object_delim2);
     return obj;
 }
